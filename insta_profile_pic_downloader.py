7ecbf25c0bb97886cf05397315ed6ecd48d29d84
ash6898 <aakashguru6898@gmail.com>
2020-08-27 13:08:05 +0530

    final commit

--- insta_profile_pic_downloader.py
+++ insta_profile_pic_downloader.py
@@ -1,21 +1,21 @@
+#importing libraries 
 import urllib.request, json
 
+#u_name gets the username whose profile picture to be downloaded
 u_name = input('Enter the username to download the instagram profile\n')
+
+#link provides user profile page data in json
 link = 'https://www.instagram.com/' + u_name + '/?__a=1'
 
+#when user exists
 try:
     with urllib.request.urlopen(link) as url:
         data = json.loads(url.read().decode())
         img_url = data['graphql']['user']['profile_pic_url_hd']
+    #saves profile picture in the same directory
     urllib.request.urlretrieve(img_url, u_name + '_insta_dp.jpg')
     print('insta dp downloaded successfully...')
+
+#when user does not exist
 except urllib.error.HTTPError:
     print('user does not exist')
-
-
-'''#Used to make requests
-import urllib.request
-
-x = urllib.request.urlopen('https://www.google.com/')
-print(x.read())
-'''
